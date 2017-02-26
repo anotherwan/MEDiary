cf1e6205506d14a2123f625c4468980112f73946
anotherwan <itsanotherwan@gmail.com>
2017-02-25 19:20:37 -0500

    removed steph's useless nav content

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -2,15 +2,9 @@ import React, {Component} from 'react';
 
 class Nav extends Component {
 
-
   render () {
     return (
-      <div className="nav">
-
-      </div>
     )
-// Nav code pliss
-
   }
 }
 
