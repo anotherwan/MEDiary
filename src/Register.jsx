09f6f67715796f96310993712ceeccfb27b79de1
Kapish <kapish.k.1@gmail.com>
2017-02-26 15:23:37 -0500

    added register inputs

--- src/Register.jsx
+++ src/Register.jsx
@@ -10,38 +10,38 @@ class Register extends Component {
             Name:
             <input type="text" name="name" />
           </label><br/>
-
           <label>
             E-mail:
             <input type="email" name="email" />
           </label><br/>
-
           <label>
             Password:
             <input type="password" name="password" />
           </label><br/>
+          <label>
+            Age:
+            <input type="text" name="age" />
+          </label> <br />
           <label>
             Gender:
             <select>
               <option value="male"> Male </option>
               <option value="female"> Female </option>
             </select>
-          </label>
-
+          </label><br />
+          <label>
+            Weight:
+            <input type="text" name="weight" />
+          </label> <br />
+          <label>
+            Height:
+            <input type="text" name="height" />
+          </label> <br />
+            <input type="submit" value="Submit" />
         </form>
-
       </div>
     )
-
-
   }
 }
 
 export default Register;
-
-
-          // password: 'human',
-          // age: 22,
-          // gender: 'male',
-          // weight: 145,
-          // height: '5ft 10in'
