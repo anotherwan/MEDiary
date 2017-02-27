6d206a8bfaa9cd2a6ab92da8fb0747433803257d
anotherwan <itsanotherwan@gmail.com>
2017-02-27 13:26:38 -0500

    added d3, react-faux-dom, and added body to main page

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,17 +1,77 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
+import ReactDOM from 'react-dom';
+import ReactFauxDOM from 'react-faux-dom';
+import * as d3 from 'd3';
+
 import './App.css';
 import maleBody from '../public/images/mainBodyGray.svg';
+import head from '../public/images/headHead.svg';
+import torso from '../public/images/torsoMan.svg';
+import arms from '../public/images/armsArm.svg';
+import legs from '../public/images/leg.svg';
 
 
 class Home extends Component {
-
   render () {
-    return (
-      <div className="home">
-        <img src={maleBody}/>
-      </div>
-    )
+    const div = new ReactFauxDOM.createElement('div');
+
+    const mainBodySVG = d3.select(div)
+      .append("svg")
+      .attr("height", "600")
+      .attr("width", "500")
+      .attr("x", 0)
+      .attr("y", 0)
+
+    mainBodySVG.append("svg:image")
+      .attr("class", "maleBody")
+      .attr("xlink:href", "images/mainBodyGray.svg")
+      .attr("width", 400)
+      .attr("height", 500)
+
+    mainBodySVG.append("defs")
+      .append ()
+
+      // .attr("viewBox", "800 800")
+      // .attr("preserveAspectRatio", "xMinYMin meet")
+      // .append("svg:image")
+      // .attr("height", 400)
+      // .attr("x", 0)
+      // .attr("y", 0);
+
+    const dashboard = d3.select(div)
+      .append("svg")
+      .attr("height", "600")
+      .attr("width", "500")
+      .attr("x", 500)
+      .attr("y", 0)
+
+
+
+
+    // svgBody.append("circle")
+    //   .attr("cx", 400)
+    //   .attr("cy", 36)
+    //   .attr("r", 35)
+    //   .style("stroke", "black")
+    //   .style("stroke-width", 1.5)
+    //   .style("fill", "transparent")
+
+
+
+    return div.toReact()
+
+
+
+
+    // return (
+    //   <div className="home">
+    //     <img src={maleBody}/>
+    //     <img src={head}/>
+    //     <img src={torso}/>
+    //     <img src={arms}/>
+    //     <img src={legs}/>
+    //   </div>
+    // )
   }
 }
 
---
+++ src/homeD3.js
@@ -0,0 +1,13 @@
+// import * as d3 from 'd3';
+// import reactDOM from 'react-dom';
+// import ReactFauxDOM from 'react-faux-dom';
+//
+// const mainBodyDiv = ReactFauxDOM.createElement('div');
+// mainBodyDiv.style.setProperty('display', 'flex');
+// mainBodyDiv.style.setProperty('justify-content', 'flex-start');
+//
+// const mainBodyImg = d3.select(mainBodyDiv).append("image")
+//   .attr("width", width + )
+//
+//
+// return MainBodySVG.toReact();
