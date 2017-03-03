45827fbc355cecdc21f59672bfc910245aeef394
anotherwan <itsanotherwan@gmail.com>
2017-03-03 17:49:15 -0500

    added navbar, aligned home page body and bubbles, worked on navbar

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import { Link } from "react-router";
+import { Row, Col } from 'react-materialize';
 
 import maleBody from '../public/images/mainBodyGray.svg';
 import headHead from '../public/images/headHead.svg';
 import torsoMan from '../public/images/torsoMan.svg';
 import armsArm from '../public/images/armsArm.svg';
 import legsLeg from '../public/images/legsLeg.svg';
+
+import Login from './Login.jsx';
 
 class Home extends Component {
 
@@ -21,24 +24,45 @@ class Home extends Component {
   }
 
   render () {
-    return (
-      <div className="home">
-        <img src={maleBody} alt="Body" />
-        <Link to="/region/head">
-          <img src={headHead} alt="Head" style={this.bubbleStyle}/>
-        </Link>
-        <Link to="/region/torso">
-          <img src={torsoMan} alt="Torso" style={this.bubbleStyle}/>
-        </Link>
-        <Link to="/region/arms">
-          <img src={armsArm} alt="Arms" style={this.bubbleStyle}/>
-        </Link>
-        <Link to="/region/legs">
-          <img src={legsLeg} alt="Legs" style={this.bubbleStyle}/>
-        </Link>
-
+    // if(localStorage.getItem('uid'))
+      return (
+        <div className="home">
+            <Row style={{'margin':'0px'}}>
+                <Col m={4} >
+                  <Row></Row>
+                  <img src={maleBody} alt="Body" style={{'height': '380px'}} />
+                </Col>
+                <Col m={2} className='m5' s={3}>
+                  <div style={{'border-bottom':'solid black 2px', 'width': '100px'}}><h6>Head</h6></div>
+                  <Link to="/region/head">
+                    <img src={headHead} alt="Head" style={this.bubbleStyle}/>
+                  </Link>
+                </Col>
+                <Col m={2} className='m5' s={3}>
+                  <div style={{'border-bottom':'solid black 2px', 'width': '100px'}}><h6>Torso</h6></div>
+                  <Link to="/region/torso">
+                    <img src={torsoMan} alt="Torso" style={this.bubbleStyle}/>
+                  </Link>
+                </Col>
+                <Col m={2} className='m5' s={3}>
+                  <div style={{'border-bottom':'solid black 2px', 'width': '100px'}}><h6>Arms</h6></div>
+                  <Link to="/region/arms">
+                    <img src={armsArm} alt="Arms" style={this.bubbleStyle}/>
+                  </Link>
+                </Col>
+                <Col m={2} className='m5' s={3}>
+                  <div style={{'border-bottom':'solid black 2px', 'width': '100px'}}><h6>Legs</h6></div>
+                  <Link to="/region/legs">
+                    <img src={legsLeg} alt="Legs" style={this.bubbleStyle}/>
+                  </Link>
+                </Col>
+          </Row>
       </div>
     )
+    // else
+    //   return (
+    //     <Login />
+    //   )
   }
 }
 
--- src/Login.jsx
+++ src/Login.jsx
@@ -1,63 +1,63 @@
 import React, {Component} from 'react';
 
 class Login extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: '',
-      password: '',
-      login_error: ''
-    };
-    this.onSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleSubmit = (event) => {
-    event.preventDefault();
-
-    this.setState({
-      login_error: ''
-    })
-
-    fetch('http://localhost:4000/login', {
-      method: 'post',
-      mode: 'cors',
-      headers: { 'Content-Type': 'application/json'},
-      body: JSON.stringify({
-        email: this.state.email,
-        password: this.state.password
-      })
-    }).then((response) => {
-      return response.json();
-    }).then((body) => {
-      switch(body.message) {
-        case 'User not found':
-          this.setState({
-            login_error: body.message
-          })
-          break;
-        case 'Email and/or password cannot be empty':
-          this.setState({
-            login_error: body.message
-          })
-          break;
-        case 'Invalid email and/or password':
-          this.setState({
-            login_error: body.message
-          })
-          break;
-        default: {
-          localStorage.setItem('uid', body.message);
-          this.props.router.push('/');
-        }
-      }
-    })
-
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     email: '',
+  //     password: '',
+  //     login_error: ''
+  //   };
+  //   this.onSubmit = this.handleSubmit.bind(this);
+  // }
+  //
+  // handleSubmit = (event) => {
+  //   event.preventDefault();
+  //
+  //   this.setState({
+  //     login_error: ''
+  //   })
+  //
+  //   fetch('http://localhost:4000/login', {
+  //     method: 'post',
+  //     mode: 'cors',
+  //     headers: { 'Content-Type': 'application/json'},
+  //     body: JSON.stringify({
+  //       email: this.state.email,
+  //       password: this.state.password
+  //     })
+  //   }).then((response) => {
+  //     return response.json();
+  //   }).then((body) => {
+  //     switch(body.message) {
+  //       case 'User not found':
+  //         this.setState({
+  //           login_error: body.message
+  //         })
+  //         break;
+  //       case 'Email and/or password cannot be empty':
+  //         this.setState({
+  //           login_error: body.message
+  //         })
+  //         break;
+  //       case 'Invalid email and/or password':
+  //         this.setState({
+  //           login_error: body.message
+  //         })
+  //         break;
+  //       default: {
+  //         localStorage.setItem('uid', body.message);
+  //         this.props.router.push('/');
+  //       }
+  //     }
+  //   })
+  //
+  // }
 
   render () {
     return (
       <div className="login">
-        <form id="login" onSubmit={this.onSubmit}>
+        {/* <form id="login" onSubmit={this.onSubmit}>
           <label>
             Email:
             <input type="email" onChange={(e) => this.setState({email: e.target.value})} value={this.state.email}/>
@@ -72,7 +72,7 @@ class Login extends Component {
         </form>
         <div >
           {this.state.login_error}
-        </div>
+        </div> */}
       </div>
     )
   }
