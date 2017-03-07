9928e54c913574fe6ef6e40b18d28e17269e2136
Kapish <kapish.k.1@gmail.com>
2017-03-07 11:02:58 -0500

    fixing home redirect

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,11 @@ import '../public/styles/App.css';
 
 import Nav from './Nav.jsx';
 import Foot from './Foot.jsx';
-import {Row, Col, Input, Button, Breadcrumb, MenuItem} from "react-materialize";
+import {Row, Col, Input, Button, Breadcrumb, MenuItem, Modal} from "react-materialize";
 import Login from './Login.jsx';
-import AboutUs from './AboutUs.jsx';
 // import Home from './Home.jsx';
 
 class App extends Component {
-
-    constructor(props) {
-    super(props);
-    this.state = {
-      showAboutUs: false,
-    };
-    this.AboutUs = this.AboutUs.bind(this);
-  }
-
-  AboutUs () {
-    this.setState({
-      showAboutUs: true,
-    })
-  }
 
 
   render() {
@@ -31,10 +16,23 @@ class App extends Component {
       <div className="App">
         <Nav />
         {this.props.children}
-        {this.state.showAboutUs ? <AboutUs /> : null}
         <Button floating fab='vertical' floating icon='list' faicon='fa fa-plus' className='red' large style={{bottom: '45px', right: '24px'}}>
-          <Button onClick={this.AboutUs} floating icon='contacts' className='green'/>
-          <Button floating icon='star' className='blue'/>
+          <Modal
+            header='About Us'
+            fixedFooter
+            trigger={
+              <Button floating icon='contacts' className='green'/>
+            }>
+            Kapish, Steph, Parshant
+          </Modal>
+          <Modal
+            header='Thanks to....'
+            fixedFooter
+            trigger={
+              <Button floating icon='star' className='blue'/>
+            }>
+            People we wanna thank in here
+          </Modal>
         </Button>
       </div>
     );
