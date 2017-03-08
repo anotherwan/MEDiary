0e67926b87a89d93f508000bb45642d14dd043e1
anotherwan <itsanotherwan@gmail.com>
2017-03-07 21:47:06 -0500

    graph now shows 4 lines! y coords still hardcoded in

--- src/Graph.jsx
+++ src/Graph.jsx
@@ -1,38 +1,34 @@
 import React, {Component} from 'react';
 import {Line} from 'react-chartjs-2';
-import Moment from 'react-moment';
-import moment from 'moment';
 
 class Graph extends Component {
   myData = {
       labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-      // datasets: [
-      //   {
-      //     label: 'Head Pains',
-      //     fill: true,
-      //     lineTension: 0.01,
-      //     backgroundColor: 'rgba(75,192,192,0.4)',
-      //     borderColor: 'rgba(75,192,192,1)',
-      //     borderCapStyle: 'butt',
-      //     borderDash: [],
-      //     borderDashOffset: 0.0,
-      //     borderJoinStyle: 'miter',
-      //     pointBorderColor: 'rgba(75,192,192,1)',
-      //     pointBackgroundColor: '#fff',
-      //     pointBorderWidth: 1,
-      //     pointHoverRadius: 5,
-      //     pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-      //     pointHoverBorderColor: 'rgba(220,220,220,1)',
-      //     pointHoverBorderWidth: 2,
-      //     pointRadius: 10,
-      //     pointHitRadius: 10,
-      //     data: [65, 59, 80, 81, 56, 55, 40, 25, 34, 51, 12, 9]
-      //   }
-      // ],
       datasets: [
         {
+          label: 'Head Pains',
+          fill: true,
+          lineTension: 0.01,
+          backgroundColor: 'rgba(75,192,192,0.4)',
+          borderColor: 'rgba(75,192,192,1)',
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: 'rgba(75,192,192,1)',
+          pointBackgroundColor: '#fff',
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+          pointHoverBorderColor: 'rgba(220,220,220,1)',
+          pointHoverBorderWidth: 2,
+          pointRadius: 8,
+          pointHitRadius: 10,
+          data: [10, 5, 0, 3, 8, 14, 18, 19, 8, 5, 2, 1]
+        },
+        {
           label: 'Torso Pains',
-          fill: false,
+          fill: true,
           lineTension: 0.01,
           backgroundColor: 'rgba(207, 92, 76, 0.4)',
           borderColor: 'rgba(208, 60, 42, 0.67)',
@@ -47,15 +43,58 @@ class Graph extends Component {
           pointHoverBackgroundColor: 'rgba(208, 60, 42, 0.67)',
           pointHoverBorderColor: 'rgba(220,220,220,1)',
           pointHoverBorderWidth: 2,
-          pointRadius: 10,
+          pointRadius: 8,
           pointHitRadius: 10,
-          data: [68, 40, 20, 35, 21, 63, 24, 54, 12, 56, 86, 24]
+          data: [1, 4, 7, 4, 6, 0, 8, 7, 3, 6, 0, 0]
+        },
+        {
+          label: 'Arm Pains',
+          fill: true,
+          lineTension: 0.01,
+          backgroundColor: 'rgba(255, 197, 51, 0.55)',
+          borderColor: 'rgba(255, 197, 51, 0.81)',
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: 'rgba(255, 197, 51, 0.81)',
+          pointBackgroundColor: '#fff',
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: 'rgba(255, 197, 51, 0.81)',
+          pointHoverBorderColor: 'rgba(220,220,220,1)',
+          pointHoverBorderWidth: 2,
+          pointRadius: 8,
+          pointHitRadius: 10,
+          data: [5, 3, 2, 6, 8, 9, 1, 0, 0, 2, 4, 5]
+        },
+        {
+          label: 'Leg Pains',
+          fill: true,
+          lineTension: 0.01,
+          backgroundColor: 'rgba(110, 189, 242, 0.64)',
+          borderColor: 'rgba(5, 155, 255, 0.64)',
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: 'rgba(5, 155, 255, 0.64)',
+          pointBackgroundColor: '#fff',
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: 'rgba(5, 155, 255, 0.64)',
+          pointHoverBorderColor: 'rgba(220,220,220,1)',
+          pointHoverBorderWidth: 2,
+          pointRadius: 8,
+          pointHitRadius: 10,
+          data: [4, 3, 6, 8, 9, 2, 5, 9, 10, 1, 0, 2]
         }
+
       ]
   }
   render() {
     return (
-      <div>
+      <div >
         <h2>Dashboard</h2>
         <Line
           data={this.myData}
@@ -63,6 +102,7 @@ class Graph extends Component {
           height="300px"
           options={{
             mainAspectRatio: false
+
           }}
         />
       </div>
